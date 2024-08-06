5ecf5abbd195696db35c2cbdc2c735859cf3738a
akhilesh1201 <akhilesh.koti@candelatech.com>
2024-08-06 12:28:30 +0530

    added no. of devices argument in qos and test_l3 and removed --webUI arg dependency for --device_list
    Signed-off-by: akhilesh1201 <akhilesh.koti@candelatech.com>

--- py-scripts/test_l3.py
+++ py-scripts/test_l3.py
@@ -5414,6 +5414,7 @@ class L3VariableTime(Realm):
             "Upstream": self.upstream_port,
             "Test Duration": self.test_duration,
             "Polling Interval": self.polling_interval,
+            "Total No. of Devices": self.station_count,
         }
 
         self.report.set_table_title("Test Configuration")
