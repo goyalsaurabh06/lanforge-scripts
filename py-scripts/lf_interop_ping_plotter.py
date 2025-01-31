d5056298255835a26084a2a7650064a5b38c7afd
Nikhita <kadali.nikhita@candelatech.com>
2025-01-31 11:35:41 +0530

    Included mac clients in  ping_plotter.py

--- py-scripts/lf_interop_ping_plotter.py
+++ py-scripts/lf_interop_ping_plotter.py
@@ -204,7 +204,7 @@ class Ping(Realm):
                 continue
             device_data = device_data['resource']
             # print(device_data)
-            if 'Apple' in device_data['hw version'] and (device_data['app-id'] != '' or device_data['app-id'] != '0' or device_data['kernel'] == ''):
+            if 'Apple' in device_data['hw version'] and (device_data['app-id'] != '') and (device_data['app-id'] != '0' or device_data['kernel'] == ''):
                 print('{} is an iOS device. Currently we do not support iOS devices.'.format(device))
             else:
                 filtered_list.append(device)
