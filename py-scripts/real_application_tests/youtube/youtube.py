3ad6112fc10f7ddaa707ea0273741c2086144dcb
Narayana-CT <narayana.pinapatruni@candelatech.com>
2025-02-26 14:02:48 +0530

    fixed youtube video not playing issue
    Signed-off-by: Narayana-CT <narayana.pinapatruni@candelatech.com>

--- py-scripts/real_application_tests/youtube/youtube.py
+++ py-scripts/real_application_tests/youtube/youtube.py
@@ -394,6 +394,9 @@ Example:
     # options.add_argument("--memory-pressure-thresholds-mb=2048")
     # options.add_experimental_option("detach", True)
     options.add_argument('--no-sandbox')
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
 
     driver = webdriver.Chrome(service=service, options=options)# Or choose the appropriate webdriver for your browser
 
