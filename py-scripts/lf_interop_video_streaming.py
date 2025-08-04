536aa594fc6efc0f72a62e0480429b6328c8fa66
litincandela <litin.ommi@candelatech.com>
2025-08-04 15:28:28 +0530

    changed graph name because duplicated with throughput

--- py-scripts/lf_interop_video_streaming.py
+++ py-scripts/lf_interop_video_streaming.py
@@ -1361,7 +1361,7 @@ class VideoStreamingTest(Realm):
                                   _xaxis_categories=self.trim_data(len(realtime_dataset['timestamp'][realtime_dataset['iteration'] == iter + 1].values.tolist()),
                                                                    realtime_dataset['timestamp'][realtime_dataset['iteration'] == iter + 1].values.tolist()),
                                   _label=['Rate'],
-                                  _graph_image_name=f"line_graph{iter}"
+                                  _graph_image_name=f"vs_line_graph{iter}"
                                   )
             graph_png = graph.build_line_graph()
             logger.info("graph name {}".format(graph_png))
