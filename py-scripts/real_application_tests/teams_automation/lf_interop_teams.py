91aa461ce9ebabfbad018e197cb1a7cf9aa3c820
sivakondri-CT <kondru.sankar@candelatech.com>
2025-11-20 11:17:33 +0530

    lf_interop_teams.py : Add support to trigger iot while running teams test
    Signed-off-by: sivakondri-CT <kondru.sankar@candelatech.com>

--- py-scripts/real_application_tests/teams_automation/lf_interop_teams.py
+++ py-scripts/real_application_tests/teams_automation/lf_interop_teams.py
@@ -37,6 +37,7 @@ import json
 import sys
 import traceback
 import glob
+import asyncio
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..'))
@@ -68,6 +69,12 @@ log.setLevel(logging.ERROR)
 
 # Import LF logger configuration module
 lf_logger_config = importlib.import_module("py-scripts.lf_logger_config")
+
+
+iot_scripts_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../local/interop-webGUI/IoT/scripts/"))
+if os.path.exists(iot_scripts_path):
+    sys.path.insert(0, iot_scripts_path)
+    from test_automation import Automation  # noqa: E402
 
 
 class TeamsAutomation(Realm):
@@ -864,6 +871,79 @@ class TeamsAutomation(Realm):
             logging.error(f"An error occurred while updating status: {e}")
 
 
+def trigger_iot(ip, port, iterations, delay, device_list, testname, increment):
+    """
+    Entry point to start the IoT test in a separate thread.
+    This function is called from the throughput test script when IoT testing
+    is enabled. It wraps the asynchronous `run_iot()`.
+    """
+    asyncio.run(run_iot(ip, port, iterations, delay, device_list, testname, increment))
+
+
+async def run_iot(ip: str = '127.0.0.1',
+                  port: str = '8000',
+                  iterations: int = 1,
+                  delay: int = 5,
+                  device_list: str = '',
+                  testname: str = '',
+                  increment: str = ''):
+    try:
+
+        if delay < 5:
+            logger.error('The minimum delay should be 5 seconds.')
+            exit(1)
+
+        if device_list != '':
+            device_list = device_list.split(',')
+        else:
+            device_list = None
+        # Parse and validate increment pattern if provided
+        if increment:
+            print("the increment is : ", increment)
+            try:
+                increment = list(map(int, increment.split(',')))
+                if any(i < 1 for i in increment):
+                    logger.error('Increment values must be positive integers')
+                    exit(1)
+            except ValueError:
+                logger.error('Invalid increment format. Please provide comma-separated integers (e.g., "1,3,5")')
+                exit(1)
+
+        testname = testname
+
+        # Ensure test name is unique (avoid overwriting previous results)
+        if testname in os.listdir('../../../../local/interop-webGUI/IoT/scripts/results/'):
+            logger.error('Test with same name already existing. Please give a different testname.')
+            exit(1)
+        automation = Automation(ip=ip,
+                                port=port,
+                                iterations=iterations,
+                                delay=delay,
+                                device_list=device_list,
+                                testname=testname,
+                                increment=increment)
+
+        # fetch the available iot devices
+        automation.devices = await automation.fetch_iot_devices()
+
+        # select the iot devices for testing
+        automation.select_iot_devices()
+
+        # run the iot test on selected devices
+        automation.run_test()
+
+        # generate the iot report
+        automation.generate_report()
+
+    except Exception as e:
+        logger.error(f"Iot Test failed: {str(e)}")
+        raise
+
+    await automation.session.close()
+
+    logger.info('Iot Test Completed.')
+
+
 def main():
     try:
 
@@ -915,6 +995,15 @@ def main():
         optional.add_argument('--do_webUI', action='store_true', help='useful to specify whether we are running through webui or cli')
         optional.add_argument('--testname', help="report directory while running test through web ui")
         optional.add_argument('--report_dir', help="report directory while running test through web ui")
+        # IOT ARGS
+        parser.add_argument('--iot_test', help="If true will execute script for iot", action='store_true')
+        optional.add_argument('--iot_ip', default='127.0.0.1', help='IP of the server')
+        optional.add_argument('--iot_port', default='8000', help='Port of the server')
+        optional.add_argument('--iot_iterations', type=int, default=1, help='Iterations to run the test')
+        optional.add_argument('--iot_delay', type=int, default=5, help='Delay in seconds between iterations (min. 5 seconds)')
+        optional.add_argument('--iot_device_list', type=str, default='', help='Entity IDs of the devices to include in testing (comma separated)')
+        optional.add_argument('--iot_testname', type=str, default='', help='Testname for reporting')
+        optional.add_argument('--iot_increment', type=str, default='', help='Comma-separated list of device counts to incrementally test (e.g., "1,3,5")')
 
         args = parser.parse_args()
 
@@ -927,6 +1016,14 @@ def main():
         if args.lf_logger_config_json:
             logger_config.lf_logger_config_json = args.lf_logger_config_json
             logger_config.load_lf_logger_config()
+        if args.iot_test:
+            iot_ip = args.iot_ip
+            iot_port = args.iot_port
+            iot_iterations = args.iot_iterations
+            iot_delay = args.iot_delay
+            iot_device_list = args.iot_device_list
+            iot_testname = args.iot_testname
+            iot_increment = args.iot_increment
 
         teams = TeamsAutomation(
             lanforge_ip=args.mgr,
@@ -960,6 +1057,27 @@ def main():
         if args.do_webUI:
             teams.path = args.report_dir
             teams.update_webui_data()
+        if args.iot_test:
+            if args.iot_iterations > 1:
+                thread = threading.Thread(target=trigger_iot, args=(iot_ip, iot_port, iot_iterations, iot_delay, iot_device_list, iot_testname, iot_increment))
+                thread.start()
+            else:
+                total_secs = args.duration * 60
+                iot_iterations = max(1, total_secs // args.iot_delay)
+                iot_thread = threading.Thread(
+                    target=trigger_iot,
+                    args=(
+                        args.iot_ip,
+                        args.iot_port,
+                        iot_iterations,
+                        args.iot_delay,
+                        args.iot_device_list,
+                        args.iot_testname,
+                        args.iot_increment
+                    ),
+                    daemon=True
+                )
+                iot_thread.start()
         teams.load_credentials()
         teams.run()
         time.sleep(10)
