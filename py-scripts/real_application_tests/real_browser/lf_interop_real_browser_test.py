74605a07d02dc74ea0e08bab50e966ae135a3fee
sivakondri-CT <kondru.sankar@candelatech.com>
2025-11-20 12:54:50 +0530

    lf_interop_real_browser.py : Add support to trigger iot while running the real browser test
    Signed-off-by: sivakondri-CT <kondru.sankar@candelatech.com>

--- py-scripts/real_application_tests/real_browser/lf_interop_real_browser_test.py
+++ py-scripts/real_application_tests/real_browser/lf_interop_real_browser_test.py
@@ -117,6 +117,11 @@ logger = logging.getLogger(__name__)
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+iot_scripts_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../local/interop-webGUI/IoT/scripts/"))
+if os.path.exists(iot_scripts_path):
+    sys.path.insert(0, iot_scripts_path)
+    from test_automation import Automation  # noqa: E402
+
 
 class RealBrowserTest(Realm):
     def __init__(self,
@@ -1939,6 +1944,79 @@ class RealBrowserTest(Realm):
         return final_eid_data, mac_data, channel_data, signal_data, ssid_data, tx_rate_data, device_names, device_type_data
 
 
+def trigger_iot(ip, port, iterations, delay, device_list, testname, increment):
+    """
+    Entry point to start the IoT test in a separate thread.
+    This function is called from the throughput test script when IoT testing
+    is enabled. It wraps the asynchronous `run_iot()`.
+    """
+    asyncio.run(run_iot(ip, port, iterations, delay, device_list, testname, increment))
+
+
+async def run_iot(ip: str = '127.0.0.1',
+                  port: str = '8000',
+                  iterations: int = 1,
+                  delay: int = 5,
+                  device_list: str = '',
+                  testname: str = '',
+                  increment: str = ''):
+    try:
+
+        if delay < 5:
+            logger.error('The minimum delay should be 5 seconds.')
+            exit(1)
+
+        if device_list != '':
+            device_list = device_list.split(',')
+        else:
+            device_list = None
+        # Parse and validate increment pattern if provided
+        if increment:
+            print("the increment is : ", increment)
+            try:
+                increment = list(map(int, increment.split(',')))
+                if any(i < 1 for i in increment):
+                    logger.error('Increment values must be positive integers')
+                    exit(1)
+            except ValueError:
+                logger.error('Invalid increment format. Please provide comma-separated integers (e.g., "1,3,5")')
+                exit(1)
+
+        testname = testname
+
+        # Ensure test name is unique (avoid overwriting previous results)
+        if testname in os.listdir('../../../../local/interop-webGUI/IoT/scripts/results/'):
+            logger.error('Test with same name already existing. Please give a different testname.')
+            exit(1)
+        automation = Automation(ip=ip,
+                                port=port,
+                                iterations=iterations,
+                                delay=delay,
+                                device_list=device_list,
+                                testname=testname,
+                                increment=increment)
+
+        # fetch the available iot devices
+        automation.devices = await automation.fetch_iot_devices()
+
+        # select the iot devices for testing
+        automation.select_iot_devices()
+
+        # run the iot test on selected devices
+        automation.run_test()
+
+        # generate the iot report
+        automation.generate_report()
+
+    except Exception as e:
+        logger.error(f"Iot Test failed: {str(e)}")
+        raise
+
+    await automation.session.close()
+
+    logger.info('Iot Test Completed.')
+
+
 def main():
     try:
 
@@ -2072,7 +2150,20 @@ def main():
         parser.add_argument("--device_csv_name", type=str, help="Specify the device csv name for pass/fail", default=None)
         parser.add_argument("--wait_time", type=int, help="Specify the time for configuration", default=60)
         parser.add_argument('--config', action='store_true', help='specify this flag whether to config devices or not')
+        # IOT ARGS
+        parser.add_argument('--iot_test', help="If true will execute script for iot", action='store_true')
+        optional.add_argument('--iot_ip', default='127.0.0.1', help='IP of the server')
 
+        optional.add_argument('--iot_port', default='8000', help='Port of the server')
+        optional.add_argument('--iot_iterations', type=int, default=1, help='Iterations to run the test')
+
+        optional.add_argument('--iot_delay', type=int, default=5, help='Delay in seconds between iterations (min. 5 seconds)')
+
+        optional.add_argument('--iot_device_list', type=str, default='', help='Entity IDs of the devices to include in testing (comma separated)')
+
+        optional.add_argument('--iot_testname', type=str, default='', help='Testname for reporting')
+
+        optional.add_argument('--iot_increment', type=str, default='', help='Comma-separated list of device counts to incrementally test (e.g., "1,3,5")')
         args = parser.parse_args()
         if args.help_summary:
             print(help_summary)
@@ -2086,6 +2177,14 @@ def main():
         if args.lf_logger_config_json:
             logger_config.lf_logger_config_json = args.lf_logger_config_json
             logger_config.load_lf_logger_config()
+        if args.iot_test:
+            iot_ip = args.iot_ip
+            iot_port = args.iot_port
+            iot_iterations = args.iot_iterations
+            iot_delay = args.iot_delay
+            iot_device_list = args.iot_device_list
+            iot_testname = args.iot_testname
+            iot_increment = args.iot_increment
 
         # Initialize an instance of RealBrowserTest with various parameters
         obj = RealBrowserTest(host=args.host,
@@ -2137,6 +2236,27 @@ def main():
         if not obj.expected_passfail_value and obj.device_csv_name is None:
             obj.config_obj.device_csv_file(csv_name="device.csv")
         obj.run_flask_server()
+        if args.iot_test:
+            if args.iot_iterations > 1:
+                thread = threading.Thread(target=trigger_iot, args=(iot_ip, iot_port, iot_iterations, iot_delay, iot_device_list, iot_testname, iot_increment))
+                thread.start()
+            else:
+                total_secs = int(args.duration) * 60
+                iot_iterations = max(1, total_secs // args.iot_delay)
+                iot_thread = threading.Thread(
+                    target=trigger_iot,
+                    args=(
+                        args.iot_ip,
+                        args.iot_port,
+                        iot_iterations,
+                        args.iot_delay,
+                        args.iot_device_list,
+                        args.iot_testname,
+                        args.iot_increment
+                    ),
+                    daemon=True
+                )
+                iot_thread.start()
         if obj.group_name and obj.profile_name and obj.file_name:
             available_resources = obj.process_group_profiles()
         else:
