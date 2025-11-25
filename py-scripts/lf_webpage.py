d847109f491140e4218f27ab287238873828c89d
litincandela <litin.ommi@candelatech.com>
2025-11-25 17:11:48 +0530

    lf_webpage.py: made some changes in ftp test to run in both fake and original robo

--- py-scripts/lf_webpage.py
+++ py-scripts/lf_webpage.py
@@ -94,7 +94,8 @@ import traceback
 import asyncio
 from typing import List, Optional
 import csv
-from lf_robo_base_class import RobotClass
+# from lf_robo_base_class import RobotClass
+from lf_base_robo import RobotClass
 
 sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../")))
 
@@ -213,6 +214,7 @@ class HttpDownload(Realm):
         self.current_coordinate = ""
         self.current_angle = 0
         self.robot_data = {}
+        self.robot_obj = {}
 
 # The 'phantom_check' will be handled within the 'get_real_client_list' function
     def get_real_client_list(self):
@@ -643,7 +645,8 @@ class HttpDownload(Realm):
         self.http_profile.stop_cx()
         # To update status of devices and remaining_time in ftp_datavalues.csv file to stopped and 0 respectively.
         if self.client_type == 'Real':
-            self.data["status"] = ["STOPPED"] * len(self.macid_list)
+            if not self.robot_test:
+                self.data["status"] = ["STOPPED"] * len(self.macid_list)
             self.data["remaining_time"] = ["0"] * len(self.macid_list)
             df1 = pd.DataFrame(self.data)
             df1.to_csv("http_datavalues.csv", index=False)
@@ -657,6 +660,15 @@ class HttpDownload(Realm):
                 else:
                     df1.to_csv(f"{self.current_coordinate}_http_datavalues.csv", index=False)
         print("Test Stop done", self.robot_data)
+
+    def update_stop_status_robot(self):
+        self.data["status"] = ["STOPPED"] * len(self.macid_list)
+        df1 = pd.DataFrame(self.data)
+        df1.to_csv("http_datavalues.csv", index=False)
+        if self.dowebgui:
+            df1.to_csv(f"{self.result_dir}/{self.current_coordinate}_http_datavalues.csv", index=False)
+        else:
+            df1.to_csv(f"{self.current_coordinate}_http_datavalues.csv", index=False)
 
     def get_layer4_data(self):
         """
@@ -711,15 +723,15 @@ class HttpDownload(Realm):
 
         return l4_dict
 
-    def monitor_for_runtime_csv(self, duration):
+    def monitor_for_runtime_csv(self, duration, rotation_angle = ""):
 
         time_now = datetime.now()
         starttime = time_now.strftime("%d/%m %I:%M:%S %p")
         # duration = self.traffic_duration
         endtime = time_now + timedelta(seconds=duration)
         end_time = endtime
-        endtime = endtime.isoformat()[0:19]
-        current_time = datetime.now().isoformat()[0:19]
+        endtime = endtime
+        current_time = datetime.now()
         self.data = {}
         self.data["client"] = self.devices_list
         # self.data["url_data"] = []
@@ -733,7 +745,86 @@ class HttpDownload(Realm):
         for port in self.port_list:
             columns = ['TIMESTAMP', 'Bytes-rd', 'total urls', 'download_rate', 'rx_rate', 'tx_rate', 'RSSI']
             individual_device_data[port] = pd.DataFrame(columns=columns)
+        test_stopped_by_user = False
+        monitor_charge_time= current_time
+        battery = 25
         while (current_time < endtime):
+            if self.robot_test:
+                # if (datetime.now() - monitor_charge_time).total_seconds() >= 5:
+                #     print("test started", current_time)
+                #     pause_start = datetime.now()
+
+                #     pause, test_stopped_by_user = self.robot_obj.wait_for_battery(battery=battery, stop=self.stop)
+                #     if test_stopped_by_user:
+                #         break
+
+                #     if pause:
+
+                #         battery-=2
+                #         reached = self.robot_obj.move_to_coordinate(self.current_coordinate,result_dir=os.path.dirname(os.path.dirname(self.result_dir)))
+                #         if not reached:
+                #             test_stopped_by_user = True
+                #             break
+
+                #         if self.rotation_enabled:
+                #             rotation_moni = self.robot_obj.rotate_angle(1,2,self.current_angle)
+                #             if not rotation_moni:
+                #                 # test_stopped_by_user = True
+                #                 break
+
+                #         # restart traffic
+                #         self.start()
+
+                #         pause_end = datetime.now()
+                #         charge_pause = pause_end - pause_start
+
+                #         endtime += charge_pause
+
+                #     monitor_charge_time = datetime.now()
+
+                # # IMPORTANT: Update loop time
+                # current_time = datetime.now()
+                # print("after endtime",endtime)
+
+
+
+                if (datetime.now() - monitor_charge_time).total_seconds() >= 150:
+                    pause_start = datetime.now()
+
+                    pause, test_stopped_by_user = self.robot_obj.wait_for_battery(stop=self.stop)
+                    if test_stopped_by_user:
+                        break
+
+                    if pause:
+
+                        reached, abort = self.robot_obj.move_to_coordinate(self.current_coordinate)
+                        if abort:
+                            test_stopped_by_user = True 
+                            break
+                        if not reached:
+                            # test_stopped_by_user = True
+                            break
+
+                        if self.rotation_enabled:
+                            rotation_moni = self.robot_obj.rotate_angle(rotation_angle,self.current_angle)
+                            if not rotation_moni:
+                                test_stopped_by_user = True
+                                break
+
+                        # restart traffic
+                        self.start()
+
+                        pause_end = datetime.now()
+                        charge_pause = pause_end - pause_start
+
+                        endtime += charge_pause
+
+                    monitor_charge_time = datetime.now()
+
+                    # IMPORTANT: Update loop time
+                    current_time = datetime.now()
+                    print("after endtime",starttime,endtime)
+
 
             # data in json format
             # data = self.json_get("layer4/list?fields=bytes-rd")
@@ -838,6 +929,7 @@ class HttpDownload(Realm):
                 if(self.robot_test):
                     df1.to_csv(f"{self.current_coordinate}_http_datavalues.csv", index=False)
             time.sleep(5)
+            test_stopped_by_user = False
             if self.dowebgui == "True":
                 with open(self.result_dir + "/../../Running_instances/{}_{}_running.json".format(self.host,
                                                                                                  self.test_name),
@@ -846,9 +938,11 @@ class HttpDownload(Realm):
                     if data["status"] != "Running":
                         print('Test is stopped by the user')
                         self.data["end_time"] = [datetime.now().strftime("%d/%m %I:%M:%S %p")] * len(self.devices_list)
+                        test_stopped_by_user = True
                         break
 
-            current_time = datetime.now().isoformat()[0:19]
+            # current_time = datetime.now().isoformat()[0:19]
+            current_time = datetime.now()
         individual_device_csv_names = []  # To store individial device csv names
         # Iterate over each port and its corresponding DataFrame in the dictionary Saving the DataFrame to CSV
         for port, df in individual_device_data.items():
@@ -861,6 +955,8 @@ class HttpDownload(Realm):
             df.to_csv("all_l4_data.csv", index=False)
         except Exception:
             logger.error("All l4 data not found")
+        
+        return test_stopped_by_user
 
     def get_all_l4_data(self):
         """
@@ -1833,32 +1929,103 @@ class HttpDownload(Realm):
             exit(1)
 
     def perform_robo(self):
-
         if(self.rotation_list[0]!=""):
             self.rotation_enabled=True
-
-        robot_obj = RobotClass()
-        robot_obj.robo_ip = "127.0.0.1:5000"  
-
+        test_stopped_by_user = False
+        self.robot_obj = RobotClass()
+        self.robot_obj.ip = self.host
+        self.robot_obj.testname = self.test_name
+        self.robot_obj.runtime_dir = self.result_dir
+        self.robot_obj.robo_ip = "127.0.0.1:5000" 
+        self.robot_obj.runtime_dir = os.path.dirname(os.path.dirname(self.result_dir))
         for coordinate in range(len(self.coordinate_list)):
-            robo_moved = robot_obj.move_to_coordinate(self.coordinate_list[coordinate],result_dir=os.path.dirname(os.path.dirname(self.result_dir)))
+            if_paused,test_stopped_by_user=self.robot_obj.wait_for_battery(25)
+
+            if test_stopped_by_user:
+                break
+            robo_moved = self.robot_obj.move_to_coordinate(self.coordinate_list[coordinate],result_dir=os.path.dirname(os.path.dirname(self.result_dir)))
+            
             if robo_moved:
                 self.current_coordinate = self.coordinate_list[coordinate]
                 # if no rotation mode
                 if not self.rotation_enabled:
                     self.start()
-                    self.monitor_for_runtime_csv(self.duration)
+                    test_stopped_by_user = self.monitor_for_runtime_csv(self.duration)
                     self.stop()
+                    self.update_stop_status_robot()
                     
                 # if rotation mode
                 else:
-                    for angle in range(len(self.rotation_list)):
-                        robo_rotated = robot_obj.rotate_angle(1,2,self.rotation_list[angle])
-                        if robo_rotated:
-                            self.current_angle = self.rotation_list[angle]
+                    for angle in self.rotation_list:
+                        self.current_angle = angle
+
+                        is_paused, test_stopped_by_user = self.robot_obj.wait_for_battery(25)
+                        if test_stopped_by_user:
+                            break
+                        
+                        rotated = self.robot_obj.rotate_angle(1,2,angle)  # depends on actual func
+                        if rotated:
+                            if test_stopped_by_user:
+                                break
                             self.start()
-                            self.monitor_for_runtime_csv(self.duration)
+                            test_stopped_by_user = self.monitor_for_runtime_csv(self.duration)
                             self.stop()
+                            self.update_stop_status_robot()
+
+                        if test_stopped_by_user:
+                            break
+
+            
+    
+    # def perform_robo(self):
+
+    #     if(self.rotation_list[0]!=""):
+    #         self.rotation_enabled=True
+
+    #     self.robot_obj = RobotClass()
+    #     self.robot_obj.robo_ip = self.robot_ip
+    #     base_dir = os.path.dirname(os.path.dirname(self.result_dir))
+    #     nav_data = os.path.join(base_dir, 'nav_data.json') # To generate nav_data.json in webgui folder
+    #     self.robot_obj.nav_data_path = nav_data
+    #     self.robot_obj.create_waypointlist()
+    #     self.robot_obj.ip = self.host
+    #     self.robot_obj.testname = self.test_name
+    #     self.robot_obj.runtime_dir = self.result_dir
+    #     test_stopped_by_user = False
+    #     for coordinate in range(len(self.coordinate_list)):
+    #         if test_stopped_by_user:
+    #             break
+    #         if_paused,test_stopped_by_user=self.robot_obj.wait_for_battery()
+    #         if test_stopped_by_user:
+    #             break
+    #         # robo_moved = self.robot_obj.move_to_coordinate(self.coordinate_list[coordinate],result_dir=os.path.dirname(os.path.dirname(self.result_dir)))
+    #         robo_moved, abort = self.robot_obj.move_to_coordinate(self.coordinate_list[coordinate])
+    #         if abort:
+    #             break
+    #         if robo_moved:
+    #             self.current_coordinate = self.coordinate_list[coordinate]
+    #             # if no rotation mode
+    #             if not self.rotation_enabled:
+    #                 self.start()
+    #                 test_stopped_by_user = self.monitor_for_runtime_csv(self.duration)
+    #                 self.stop()
+    #                 self.update_stop_status_robot()
+                    
+    #             # if rotation mode
+    #             else:
+    #                 rotation_list=self.robot_obj.angles_to_radians(self.rotation_list)
+    #                 for angle in range(len(rotation_list)):
+    #                     is_paused, test_stopped_by_user = self.robot_obj.wait_for_battery()
+    #                     if test_stopped_by_user:
+    #                         break
+    #                     robo_rotated = self.robot_obj.rotate_angle(rotation_list[angle], self.rotation_list[angle])
+    #                     if robo_rotated:
+    #                         self.current_angle = self.rotation_list[angle]
+    #                         self.start()
+    #                         test_stopped_by_user = self.monitor_for_runtime_csv(self.duration, rotation_list[angle])
+    #                         self.stop()
+    #                         self.update_stop_status_robot()
+
 
 def validate_args(args):
     if args.expected_passfail_value and args.device_csv_name:
