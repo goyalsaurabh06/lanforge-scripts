21481d7153d51da60c68d74a9f65ed416a060a97
Sidartha-CT <neelapu.sidartha@candelatech.com>
2026-01-20 17:09:11 +0530

    robo final changes

--- py-scripts/lf_interop_port_reset_test.py
+++ py-scripts/lf_interop_port_reset_test.py
@@ -51,7 +51,6 @@ from datetime import datetime  # noqa: F811
 import pandas as pd
 import matplotlib.pyplot as plt
 import logging
-# from lf_robo_base_class import RobotClass
 from lf_base_robo import RobotClass
 import asyncio
 
@@ -138,6 +137,7 @@ class InteropPortReset(Realm):
         self.coordinate_df = {}
         self.get_live_view = get_live_view
         self.total_floors = total_floors
+        self.robo_test_stopped = False
         self.robot_ip = robot_ip
         self.robot_port = robot_port
         self.coordinate = coordinate
@@ -174,15 +174,7 @@ class InteropPortReset(Realm):
         real_device_data = self.base_interop_profile.devices_data
         if len(self.real_sta_list) == 0:
             logging.error('There are no real devices in this testbed. Aborting the test.')
-            if self.dowebgui:
-                self.result_df = pd.DataFrame.from_dict(self.result_df, orient="index")
-                self.result_df["Status"] = "Stopped"
-                if self.robot_test:
-                    self.result_df.to_csv(f"{self.result_dir}/overall_reset_{self.coordinate_list[0]}.csv", index=False)
-                else:
-                    self.result_df.to_csv(f"{self.result_dir}/overall_reset.csv", index=False)
-                pass
-            exit(0)
+            raise RuntimeError("here are no real devices in this testbed. Aborting the test.")
         logging.info(f"{self.real_sta_list}")
 
         for sta_name in self.real_sta_list:
@@ -726,13 +718,6 @@ class InteropPortReset(Realm):
 
                 for key in keys_to_delete:
                     del reset_dict[key]
-                if self.robot_test:
-                    self.result_df.to_csv(f"{self.report_path}/overall_reset_{self.current_coordinate}.csv", index=False)
-                    self.result_df.to_csv(f"{self.result_dir}/overall_reset_{self.current_coordinate}.csv", index=False)
-                else:
-                    self.result_df.to_csv(f"{self.report_path}/overall_reset.csv", index=False)
-                    self.result_df.to_csv(f"{self.result_dir}/overall_reset.csv", index=False)
-                
 
                 break
         logging.info(f"Final Reset Count Dictionary for all clients: {reset_dict}")
@@ -779,15 +764,7 @@ class InteropPortReset(Realm):
             if len(self.adb_device_list) == 0 and len(self.base_interop_profile.windows_list) == 0 and len(self.base_interop_profile.linux_list) == 0 and len(self.base_interop_profile.mac_list) == 0:
                 logging.info("There is no active devices please check system.")
                 logging.info('Aborting the test.')
-                if self.dowebgui:
-                    self.result_df = pd.DataFrame.from_dict(self.result_df, orient="index")
-                    self.result_df["Status"] = "Stopped"
-                    if self.robot_test:
-                        self.result_df.to_csv(f"{self.result_dir}/overall_reset_{self.current_coordinate}.csv", index=False)
-                    else:
-                        self.result_df.to_csv(f"{self.result_dir}/overall_reset.csv", index=False)
-                    pass
-                exit(1)
+                raise RuntimeError("There is no active devices please check system.")
             else:
                 for i in range(len(self.adb_device_list)):
                     self.phn_name.append(self.adb_device_list[i].split(".")[2])
@@ -864,7 +841,7 @@ class InteropPortReset(Realm):
                     self.robot_obj.runtime_dir = self.result_dir
                     test_stopped_by_user = False
                     for coordinate in range(len(self.coordinate_list)):
-                        if test_stopped_by_user:
+                        if test_stopped_by_user or self.robo_test_stopped:
                             break
                         # robo_moved = self.robot_obj.move_to_coordinate(self.coordinate_list[coordinate])
                         # if_paused, test_stopped_by_user = self.robot_obj.wait_for_battery()
@@ -893,7 +870,7 @@ class InteropPortReset(Realm):
                                     self.port_reset_data[self.coordinate_list[coordinate]][self.rotation_list[angle]] = {'reset_dict': reset_dict, 'test_duration': test_duration}
                                     print("sleeping 15 secs to update")
                                     time.sleep(15)
-                                    if test_stopped_by_user:
+                                    if test_stopped_by_user or self.robo_test_stopped:
                                         break
 
 
@@ -1434,8 +1411,15 @@ class InteropPortReset(Realm):
         for coordinate in range(len(self.coordinate_list)):
             if self.rotation_enabled:
                 for angle in range(len(self.rotation_list)):
-                    if 'reset_dict' in self.port_reset_data[self.coordinate_list[coordinate]][self.rotation_list[angle]] and self.port_reset_data[self.coordinate_list[coordinate]][self.rotation_list[angle]]['reset_dict'] != {}:
-                        reset_dict = self.port_reset_data[self.coordinate_list[coordinate]][self.rotation_list[angle]]['reset_dict']
+                    coord_key = self.coordinate_list[coordinate]
+                    angle_key = self.rotation_list[angle]
+
+                    if (
+                        coord_key in self.port_reset_data and
+                        angle_key in self.port_reset_data[coord_key] and
+                        'reset_dict' in self.port_reset_data[coord_key][angle_key]
+                    ):
+                        reset_dict = self.port_reset_data[coord_key][angle_key]['reset_dict']
                     else:
                         continue
                     self.lf_report.set_obj_html(_obj_title=f"Coordinate: {self.coordinate_list[coordinate]} | Rotation Angle: {self.rotation_list[angle]}°",
@@ -1499,13 +1483,18 @@ class InteropPortReset(Realm):
                     self.lf_report.set_table_dataframe(test_setup)
                     self.lf_report.build_table()
             else:
+                coord_key = self.coordinate_list[coordinate]
+
+                if (
+                    coord_key in self.port_reset_data and
+                    'reset_dict' in self.port_reset_data[coord_key]
+                ):
+                    reset_dict = self.port_reset_data[coord_key]['reset_dict']
+                else:
+                    continue
                 self.lf_report.set_obj_html(_obj_title=f"Coordinate: {self.coordinate_list[coordinate]}",
                                     _obj="")
                 self.lf_report.build_objective()
-                if 'reset_dict' in self.port_reset_data[self.coordinate_list[coordinate]] and self.port_reset_data[self.coordinate_list[coordinate]]['reset_dict'] != {}:
-                    reset_dict = self.port_reset_data[self.coordinate_list[coordinate]]['reset_dict']
-                else:
-                    continue
                 graph_suffix = "{}".format(self.coordinate_list[coordinate])
                 graph1 = self.generate_overall_graph(reset_dict=reset_dict, figsize=(13, 5), _alignmen=None, bar_width=0.5,
                                                  _legend_loc="upper center", _legend_ncol=6, _legend_fontsize=10,
