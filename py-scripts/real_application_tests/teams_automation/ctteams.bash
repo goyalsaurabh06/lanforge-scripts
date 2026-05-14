673ca94794469c19cbea6712063de85fde39d37c
Narayana-CT <narayana.pinapatruni@candelatech.com>
2026-05-14 12:57:53 +0530

    ctteams.bash: using virtual environement
    Signed-off-by: Narayana-CT <narayana.pinapatruni@candelatech.com>

--- py-scripts/real_application_tests/teams_automation/ctteams.bash
+++ py-scripts/real_application_tests/teams_automation/ctteams.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 # flake8: noqa
-# Usage: ./ctteams.bash <IP> <TYPE>
+# Usage: ./ctteams.bash <IFACE> <IP> <TYPE> (Linux)
+# Usage: ./ctteams.bash <IP> <TYPE> (Mac)
 
 if [[ "$(uname)" == "Linux" ]]; then
     # Linux: Terminate Chrome processes
-    pkill -f chrome 
+    pkill -f chrome
     pkill -f chromedriver
 
     sleep 5
 
+    VENV_PYTHON="/home/lanforge/venv/bin/python"
     # Assign command line arguments to variables
     IFACE=$1
     IP=$2
@@ -21,38 +23,42 @@ if [[ "$(uname)" == "Linux" ]]; then
     # Display is set to the VNC display.
 
     YTENV="--env SERVERS_CSV=$SERVERS_CSV --env LOCAL_DEV=$LOCAL_DEV --env LD_PRELOAD=$LD_PRELOAD"
-    if [ "$TYPE" == "host" ]; then
-        DISPLAY=$DISPLAY ./vrf_exec.bash $IFACE "python3 teams_host.py --ip $IP $YTENV" 2>&1 | tee teams_host.log
-
     
+    if [ "$TYPE" == "host" ]; then
+        # Use VENV_PYTHON and pass arguments exactly like the example
+        DISPLAY=$DISPLAY ./vrf_exec.bash "$IFACE" "$VENV_PYTHON" teams_host.py --ip "$IP" $YTENV
+
     elif [ "$TYPE" == "client" ]; then
-        DISPLAY=$DISPLAY ./vrf_exec.bash $IFACE "python3 teams_client.py --ip $IP $YTENV" 2>&1 | tee teams_client.log
+        DISPLAY=$DISPLAY ./vrf_exec.bash "$IFACE" "$VENV_PYTHON" teams_client.py --ip "$IP" $YTENV
     else
         echo "Invalid TYPE specified. Please use 'host' or 'client'."
         exit 1
     fi
 
     # Linux: Terminate Chrome processes
-    pkill -f chrome 
+    pkill -f chrome
     pkill -f chromedriver
 
 elif [[ "$(uname)" == "Darwin" ]]; then
     # macOS: Terminate Chrome processes
-    pkill -f "Google Chrome" 
+    pkill -f "Google Chrome"
     pkill -f chromedriver
 
     sleep 5
+    
+    # Define the macOS venv path
+    VENV_PYTHON="/Users/lanforge/venv/bin/python"
 
     # Assign command line arguments to variables
-
     IP=$1
     TYPE=$2
 
     if [ "$TYPE" == "host" ]; then
-        python3 teams_host.py --ip $IP 2>&1 | tee teams_host.log
+        # Execute using the VENV interpreter
+        "$VENV_PYTHON" teams_host.py --ip "$IP"
 
     elif [ "$TYPE" == "client" ]; then
-        python3 teams_client.py --ip $IP 2>&1 | tee teams_client.log
+        "$VENV_PYTHON" teams_client.py --ip "$IP"
     else
         echo "Invalid TYPE specified. Please use 'host' or 'client'."
         exit 1
