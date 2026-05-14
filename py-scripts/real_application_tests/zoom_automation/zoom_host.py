3f5b8f7123acebb85f1aa31e249dda6e85497011
Narayana-CT <narayana.pinapatruni@candelatech.com>
2026-05-14 11:37:58 +0530

    zoom_host.py: Add audio_stats/video_stats init, remove hardcoded path

--- py-scripts/real_application_tests/zoom_automation/zoom_host.py
+++ py-scripts/real_application_tests/zoom_automation/zoom_host.py
@@ -34,10 +34,11 @@ class ZoomHost:
         self.end_time = None
         self.tz = pytz.timezone('Asia/Kolkata')
         self.hostname = socket.gethostname()
-        self.path = "/home/lanforge/lanforge-scripts/py-scripts/zoom_automation/test_results/"
         self.audio = True
         self.video = True
         self.stop_signal = False
+        self.audio_stats = ["0", "0", "0", "0", "0", "0", "0", "0"]
+        self.video_stats = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]
 
     def setupdriver(self):
         chrome_options = Options()
